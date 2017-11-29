b17d5c8646920c8dfbacc3bee79191ef73cff58f
minsol <sologubas5>
2017-11-29 21:25:04 +0200

    Uzduotis 9

---
+++ src/lt/kaunascoding/lt/Uzduotis10.java
@@ -0,0 +1,12 @@
+package lt.kaunascoding.lt;
+
+public class Uzduotis10 {
+
+    // UZDUOTIS
+    // Sukurti skaičiuotuvo klasę, kurioje realizuoti statinius metodus sumai, skirtumui,
+    //    sandaugai. Per parametrus bus naudojami sveiko tipo skaičiai.
+
+    private class skaiciuotuvas {
+
+    }
+}
